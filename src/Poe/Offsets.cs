f95f0966289436a12fa81501edca67643053d0fd
TehCheat <thecheatuc@gmail.com>
2016-07-06 17:27:51 -0400

    Updated offsets for 2.3.1b.  AreaChangeCount updated.

--- src/Poe/Offsets.cs
+++ src/Poe/Offsets.cs
@@ -6,7 +6,7 @@ namespace PoeHUD.Poe
 {
     public class Offsets
     {
-        public static Offsets Regular = new Offsets { IgsOffset = 0, IgsDelta = 0, ExeName = "PathOfExile", AreaChangeCount = 0xA16FD8 };
+        public static Offsets Regular = new Offsets { IgsOffset = 0, IgsDelta = 0, ExeName = "PathOfExile", AreaChangeCount = 0xA10FD8 };
         public static Offsets Steam = new Offsets { IgsOffset = 0x1C, IgsDelta = 0x4, ExeName = "PathOfExileSteam", AreaChangeCount = 0xA47118 };
         /* offsets from some older steam version:
 		 	Base = 8841968;
