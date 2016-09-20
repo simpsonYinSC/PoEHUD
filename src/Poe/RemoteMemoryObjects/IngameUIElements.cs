8109cb321cd6b597ed55f11955304239b800d769
TehCheat <thecheatuc@gmail.com>
2016-09-20 09:07:13 -0400

    Fixed Camera and UI offsets.

--- src/Poe/RemoteMemoryObjects/IngameUIElements.cs
+++ src/Poe/RemoteMemoryObjects/IngameUIElements.cs
@@ -7,8 +7,8 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
     public class IngameUIElements : RemoteMemoryObject
     {
         public Element QuestTracker => ReadObjectAt<Element>(0x9F0 + Offsets.GarenaTWDelta);
-        public Element OpenLeftPanel => ReadObjectAt<Element>(0xA18 + Offsets.GarenaTWDelta);
-        public Element OpenRightPanel => ReadObjectAt<Element>(0xA1C + Offsets.GarenaTWDelta);
+        public Element OpenLeftPanel => ReadObjectAt<Element>(0xA20 + Offsets.GarenaTWDelta);
+        public Element OpenRightPanel => ReadObjectAt<Element>(0xA24 + Offsets.GarenaTWDelta);
         public Element InventoryPanel => ReadObjectAt<Element>(0xA34 + Offsets.GarenaTWDelta);
         public Element TreePanel => ReadObjectAt<Element>(0xA48 + Offsets.GarenaTWDelta);
         public Element AtlasPanel => ReadObjectAt<Element>(0xA4C + Offsets.GarenaTWDelta);
