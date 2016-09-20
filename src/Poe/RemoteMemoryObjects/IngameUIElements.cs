aeda8ff1390adb7fef2d26e60fbfd5d4e997b6cd
TehCheat <thecheatuc@gmail.com>
2016-09-20 08:58:52 -0400

    Garena 2.4.0f updated offsets.

--- src/Poe/RemoteMemoryObjects/IngameUIElements.cs
+++ src/Poe/RemoteMemoryObjects/IngameUIElements.cs
@@ -6,22 +6,22 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 {
     public class IngameUIElements : RemoteMemoryObject
     {
-        public Element QuestTracker => ReadObjectAt<Element>(0x9F0 + Offsets.GarenaTWDelta);
-        public Element OpenLeftPanel => ReadObjectAt<Element>(0xA18 + Offsets.GarenaTWDelta);
-        public Element OpenRightPanel => ReadObjectAt<Element>(0xA1C + Offsets.GarenaTWDelta);
-        public Element InventoryPanel => ReadObjectAt<Element>(0xA34 + Offsets.GarenaTWDelta);
-        public Element TreePanel => ReadObjectAt<Element>(0xA48 + Offsets.GarenaTWDelta);
-        public Element AtlasPanel => ReadObjectAt<Element>(0xA4C + Offsets.GarenaTWDelta);
-        public Map Map => ReadObjectAt<Map>(0xA64 + Offsets.GarenaTWDelta);
+        public Element QuestTracker => ReadObjectAt<Element>(0x9F4 + Offsets.GarenaTWDelta);
+        public Element OpenLeftPanel => ReadObjectAt<Element>(0xA1C + Offsets.GarenaTWDelta);
+        public Element OpenRightPanel => ReadObjectAt<Element>(0xA20 + Offsets.GarenaTWDelta);
+        public Element InventoryPanel => ReadObjectAt<Element>(0xA38 + Offsets.GarenaTWDelta);
+        public Element TreePanel => ReadObjectAt<Element>(0xA4C + Offsets.GarenaTWDelta);
+        public Element AtlasPanel => ReadObjectAt<Element>(0xA50 + Offsets.GarenaTWDelta);
+        public Map Map => ReadObjectAt<Map>(0xA68 + Offsets.GarenaTWDelta);
         public IEnumerable<ItemsOnGroundLabelElement> ItemsOnGroundLabels
         {
             get
             {
-                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xA68 + Offsets.GarenaTWDelta);
+                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xA6C + Offsets.GarenaTWDelta);
                 return itemsOnGroundLabelRoot.Children;
             }
         }
-        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xB34 + Offsets.GarenaTWDelta);
-        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xB44 + Offsets.GarenaTWDelta);
+        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xB38 + Offsets.GarenaTWDelta);
+        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xB48 + Offsets.GarenaTWDelta);
     }
 }
