892e079e15232073acceb64e74835670a813ec99
TehCheat <thecheatuc@gmail.com>
2017-03-18 11:49:45 -0400

    Rebased Garena on the x64 branch.  Updated Offsets for 2.6.0.

--- src/Poe/RemoteMemoryObjects/IngameUIElements.cs
+++ src/Poe/RemoteMemoryObjects/IngameUIElements.cs
@@ -5,23 +5,23 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 {
     public class IngameUIElements : RemoteMemoryObject
     {
-        public Element QuestTracker => ReadObjectAt<Element>(0xB58);
-        public Element OpenLeftPanel => ReadObjectAt<Element>(0xBA0);
-        public Element OpenRightPanel => ReadObjectAt<Element>(0xBA8);
-        public Elements.Inventory InventoryPanel => ReadObjectAt<Elements.Inventory>(0xBD8);
-        public Element TreePanel => ReadObjectAt<Element>(0xC08);
-        public Element AtlasPanel => ReadObjectAt<Element>(0xC10);
-        public Map Map => ReadObjectAt<Map>(0xC40);
+        public Element QuestTracker => ReadObjectAt<Element>(0xB68);
+        public Element OpenLeftPanel => ReadObjectAt<Element>(0xBA8);
+        public Element OpenRightPanel => ReadObjectAt<Element>(0xBB0);
+        public Elements.Inventory InventoryPanel => ReadObjectAt<Elements.Inventory>(0xBE0);
+        public Element TreePanel => ReadObjectAt<Element>(0xC10);
+        public Element AtlasPanel => ReadObjectAt<Element>(0xC18);
+        public Map Map => ReadObjectAt<Map>(0xC48);
         public IEnumerable<ItemsOnGroundLabelElement> ItemsOnGroundLabels
         {
             get
             {
-                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xC48);
+                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xC50);
                 return itemsOnGroundLabelRoot.Children;
             }
         }
-        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xDF0);
-        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xE10);
+        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xDF8);
+        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xE18);
     }
 }
 
